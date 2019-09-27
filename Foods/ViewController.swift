04b1d5cd8a347d9f7934bbe68f132a15f79fa90b
Dale Musser <dale@dalemusser.com>
2019-09-27 13:41:01 -0700

    First Commit

---
+++ Foods/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Foods
+//
+//  Created by Dale Musser on 9/27/19.
+//  Copyright © 2019 Dale Musser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
